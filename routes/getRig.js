4a2392e471990ef21e2a0637453a2168f33000dc
JSDRAKE <jsdrake@jsdrake.com.ar>
2023-06-22 14:44:00 -0300

    no recuerdo

--- routes/getRig.js
+++ routes/getRig.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const getRigsController = require('../controllers/getRigsController')
 
+
 router.get('/', getRigsController.getRigs)
 
 module.exports = router
